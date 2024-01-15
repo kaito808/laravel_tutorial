02038ead0bfc6ee463a05a1f21b0abc9e455a8e6
Kaito <107011546+kaito808@users.noreply.github.com>
2024-01-16 05:27:33 +0900

    Add files via upload

---
+++ storage/framework/views/01ccac8a59a377089851186e1e184bb1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ToDo App</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <header>
+    <nav class="my-navbar">
+      <a class="my-navbar-brand" href="/">ToDo App</a>
+    </nav>
+  </header>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col col-md-offset-3 col-md-6">
+          <nav class="panel panel-default">
+            <div class="panel-heading">フォルダを追加する</div>
+            <div class="panel-body">
+              <?php if($errors->any()): ?>
+              <div class="alert alert-danger">
+                <ul>
+                  <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $message): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li><?php echo e($message); ?></li>
+                  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </ul>
+              </div>
+              <?php endif; ?>
+              <form action="<?php echo e(route('folders.create')); ?>" method="post">
+                <?php echo csrf_field(); ?>
+                <div class="form-group">
+                  <label for="title">フォルダ名</label>
+                  <input type="text" class="form-control" name="title" id="title" value="<?php echo e(old('title')); ?>" />
+                </div>
+                <div class="text-right">
+                  <button type="submit" class="btn btn-primary">送信</button>
+                </div>
+              </form>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </main>
+</body>
+</html><?php /**PATH /Users/kaijp/Desktop/todo/resources/views/folders/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e3f60722ca4dab9c10f8c561da058eb2.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+<?php /**PATH /Users/kaijp/Desktop/todo/resources/views/share/flatpickr/styles.blade.php ENDPATH**/ ?>
